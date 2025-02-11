e353eb6c9adbc228ea15a7bbc5795a59bd839a80
h3770k1k1 <amagdarass@gmail.com>
2025-02-11 21:44:29 +0100

    refactor board, taskcolumnwrapper

--- src/components/TaskColumnWrapper.js
+++ src/components/TaskColumnWrapper.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Grid } from '@mui/material';
 import TaskColumn from './TaskColumn';
 import columnsConfig from '../lib/columnsConfig';
+import { handleDropTask, handleAddTask, handleCloseTask, handleTaskDescriptionChange } from '../scripts/taskHandlers';
 
-const TaskColumnWrapper = ({ title, tasks, onAddTask, onCloseTask, onDropTask, onTaskDescriptionChange, columnKey }) => {
+const TaskColumnWrapper = ({ columnKey, tasks, setTasks }) => {
   const columnData = columnsConfig[columnKey] || { name: "Unknown", color: "#FFFFFF" };
 
   return (
       <Grid item xs={3}>
         <TaskColumn
             title={columnData.name}
-            tasks={tasks}
-            onAddTask={onAddTask}
-            onCloseTask={onCloseTask}
+            tasks={tasks[columnKey]}
+            onAddTask={() => handleAddTask(tasks, setTasks, columnKey)}
+            onCloseTask={(taskId) => handleCloseTask(tasks, setTasks, columnKey, taskId)}
+            onDropTask={(taskId) => handleDropTask(taskId, columnKey, tasks, setTasks)}
             bgColor={columnData.color}
-            onDropTask={onDropTask}
             columnKey={columnKey}
-            onTaskDescriptionChange={onTaskDescriptionChange}
+            onTaskDescriptionChange={(taskId, newDescription) =>
+                handleTaskDescriptionChange(tasks, setTasks, columnKey, taskId, newDescription)
+            }
         />
       </Grid>
   );
--- src/views/Board.js
+++ src/views/Board.js
@@ -6,48 +6,40 @@ import { useNavigate } from "react-router-dom";
 import SaveBoardButton from "../components/SaveBoardButton";
 import BoardNameField from '../components/BoardNameField';
 import useBoard from '../scripts/useBoard';
-import { handleDropTask, handleAddTask, handleCloseTask, handleTaskDescriptionChange } from '../scripts/taskHandlers';
-import columnsConfig from '../lib/columnsConfig';
 
 const Board = () => {
-  const navigate = useNavigate();
-  const theme = useTheme();
-  const { user } = useAuth();
-  const { boardId, boardName, setBoardName, tasks, setTasks } = useBoard();  // Include boardId
+    const navigate = useNavigate();
+    const theme = useTheme();
+    const { user } = useAuth();
+    const { boardId, boardName, setBoardName, tasks, setTasks } = useBoard();
 
-  return (
-      <Container sx={{ width: '70%', height: '100%', padding: '20px' }}>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-start', marginTop: '0.5vh' }}>
-          <BoardNameField boardName={boardName} setBoardName={setBoardName} />
-        </Box>
-        <Grid container spacing={3} sx={{ marginTop: '20px' }}>
-          {Object.keys(tasks).map((columnKey) => (
-              <TaskColumnWrapper
-                  key={columnKey}
-                  title={columnsConfig[columnKey]?.name || columnKey}
-                  tasks={tasks[columnKey]}
-                  onAddTask={() => handleAddTask(tasks, setTasks, columnKey)}
-                  onCloseTask={(taskId) => handleCloseTask(tasks, setTasks, columnKey, taskId)}
-                  onDropTask={(taskId) => handleDropTask(taskId, columnKey, tasks, setTasks)}
-                  columnKey={columnKey}
-                  onTaskDescriptionChange={(taskId, newDescription) =>
-                      handleTaskDescriptionChange(tasks, setTasks, columnKey, taskId, newDescription)
-                  }
-              />
-          ))}
-        </Grid>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <SaveBoardButton
-              boardId={boardId}  // Now boardId is available here
-              boardName={boardName}
-              tasks={tasks}
-              user={user}
-              navigate={navigate}
-              theme={theme}
-          />
-        </Box>
-      </Container>
-  );
+    return (
+        <Container sx={{ width: '70%', height: '100%', padding: '20px' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start', marginTop: '0.5vh' }}>
+                <BoardNameField boardName={boardName} setBoardName={setBoardName} />
+            </Box>
+            <Grid container spacing={3} sx={{ marginTop: '20px' }}>
+                {Object.keys(tasks).map((columnKey) => (
+                    <TaskColumnWrapper
+                        key={columnKey}
+                        columnKey={columnKey}
+                        tasks={tasks}
+                        setTasks={setTasks}  // Pass these only once
+                    />
+                ))}
+            </Grid>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <SaveBoardButton
+                    boardId={boardId}
+                    boardName={boardName}
+                    tasks={tasks}
+                    user={user}
+                    navigate={navigate}
+                    theme={theme}
+                />
+            </Box>
+        </Container>
+    );
 };
 
 export default Board;
