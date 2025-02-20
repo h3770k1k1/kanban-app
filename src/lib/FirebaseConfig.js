db598d7c346f2179124d4a07e9832a51e462bc8c
h3770k1k1 <amagdarass@gmail.com>
2025-02-20 14:04:59 +0100

    api key file update

--- src/lib/FirebaseConfig.js
+++ src/lib/FirebaseConfig.js
@@ -1,10 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import  {API_KEY} from "./key.js"
 
 const FirebaseConfig = {
-    apiKey: API_KEY,
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "kanban-app-7e180.firebaseapp.com",
     projectId: "kanban-app-7e180",
     storageBucket: "kanban-app-7e180.firebasestorage.app",
